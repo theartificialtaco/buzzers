3acfc66c89add8a9ad6251f5093673b5b1deaff5
theartificialtaco <rashad.rchowdhury@gmail.com>
2023-10-01 13:03:14 +0600

    songTwo

--- songs/songs.ino
+++ songs/songs.ino
@@ -39,5 +39,26 @@ void songOne(){
 }
 
 void songTwo(){
-  
+  tone(buzzer, 523.25, 500);
+  delay(500*1.3);
+  tone(buzzer, 587.33, 500);
+  delay(500*1.3);
+  tone(buzzer, 392.00, 250);
+  delay(250*1.3);
+  tone(buzzer, 587.33, 500);
+  delay(500*1.3);
+  tone(buzzer, 659.25, 500);
+  delay(500*1.3);
+  tone(buzzer, 783.99, 125);
+  delay(125*1.3);
+  tone(buzzer, 698.46, 125);
+  delay(125*1.3);
+  tone(buzzer, 659.25, 125);
+  delay(125*1.3);
+  tone(buzzer, 523.25, 500);
+  delay(500*1.3);
+  tone(buzzer, 587.33, 500);
+  delay(500*1.3);
+  tone(buzzer, 392.00, 1500);
+  delay(1500*1.3);
 }
